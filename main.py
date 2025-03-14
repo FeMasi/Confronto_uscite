62a8cd855f375bbb93cbe49fc49a09839c9a0b73
Federico <federico.masiello97@gmail.com>
2025-03-14 16:26:01 +0100

    ready to try

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ df['MESE_NUM'] = df['MESE'].map(month_map)
 
 # Selezione multipla di clienti
 clienti = df['CLIENTE'].unique()
-clienti_selezionati = st.multiselect("Seleziona uno o più clienti", clienti, default=clienti[22])
+clienti_selezionati = st.multiselect("Seleziona uno o più clienti", clienti, default=clienti[21])
 
 
 # Selezione multipla di mesi per grafici per cliente
