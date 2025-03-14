49d28c59092df6611190ae38d383b77c502fac5d
Federico <federico.masiello97@gmail.com>
2025-03-14 15:04:17 +0100

    ready to try

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+
 # Caricamento dati
 st.title("Dashboard Comparativa Fatturato e Quantità")
 uploaded_file = st.file_uploader("Carica il file Excel", type=["xlsx"])
